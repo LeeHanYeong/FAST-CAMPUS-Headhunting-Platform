a4994f5fb59ad498266fe434401a6be36ee9299a
LeeHanYeong <dev@lhy.kr>
2018-12-17 01:41:06 +0900

    admin update

--- app/administrator/admin.py
+++ app/administrator/admin.py
@@ -2,6 +2,13 @@ from django.contrib import admin
 
 from .models import StaticContent, Company, Service
 
-admin.site.register(Company)
+
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'service', 'url')
+    list_editable = ('type', 'service', 'url')
+    # sortable_by = ('name',)
+
+
+admin.site.register(Company, CompanyAdmin)
 admin.site.register(Service)
 admin.site.register(StaticContent)
